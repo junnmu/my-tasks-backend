c1a8e21572a9466e972aaa91b71d11de7c4fb4ec
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2022-01-12 03:33:38 -0300

    fix user service annotation

--- src/main/java/com/example/mytasksbackend/user/UserService.java
+++ src/main/java/com/example/mytasksbackend/user/UserService.java
@@ -1,10 +1,12 @@
 package com.example.mytasksbackend.user;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
+@Service
 public class UserService implements UserServiceI {
     private UserRepository userRepository;
     private UserConfig userConfig;
--- src/main/java/com/example/mytasksbackend/user/UserServiceI.java
+++ src/main/java/com/example/mytasksbackend/user/UserServiceI.java
@@ -1,10 +1,7 @@
 package com.example.mytasksbackend.user;
 
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
 public interface UserServiceI {
     List<User> findAll();
     User save(UserReq userReq);
